a81d4b463e1e8d32ead9f90e616a45878a3e8c5f
Konstantin Komovits <konsta.2106@gmail.com>
2020-06-04 23:37:40 +0300

    Removes dots inside <> tags in xml in entsoe.js plugin

--- polku-connector/connector-dev/config/plugins/entsoe.js
+++ polku-connector/connector-dev/config/plugins/entsoe.js
@@ -1,16 +1,21 @@
 "use strict";
 var parser = require('fast-xml-parser');
+var parseString = require('xml2js').parseString;
+
 /**
  *
  * @param {Object} body
  * @return {Object}
  */
 const dataManipulation = (body) => {
+    let xml = body
+    xml = xml.replace(/\.(?=[^<>]*>)/g, '')
+    console.log('xml text', xml)
     var jsonObj = parser.parse(body);
-//   console.log('inside restjs parsebody', jsonObj.Publication_MarketDocument.TimeSeries.Period.Point[0])
-    // console.log('inside restjs parsebody', jsonObj)
+    console.log('inside entsoe.js plugin dataManipulation', jsonObj.Publication_MarketDocument.TimeSeries.Period.Point)
+    // console.log('inside restjs parsebody', jsonObj.Publication_MarketDocument.TimeSeries.Period.Point[0])
+    // console.log('inside restjs parsebody', jsonObj.Publication_MarketDocument.TimeSeries)
     return jsonObj.Publication_MarketDocument.TimeSeries.Period.Point;
-    
 };
 
 /**
